4982f4ba1e41132e33e547b4c4465f4f41a6a280
Abdalluh Mostafa <abdalluh.mostafa@gmail.com>
2024-05-11 01:36:33 +0300

    fix sql

--- lemp-newproject.sh
+++ lemp-newproject.sh
@@ -13,6 +13,7 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm i -g yarn
 sudo apt install zip unzip -y
+sudo npm install pm2 -g
 
 
 mkdir /var/www/$APP_USER
@@ -26,7 +27,7 @@ cat > /etc/nginx/sites-available/backend.$DOMAIN_NAME <<EOF
 server {
 
     server_name  	backend.$DOMAIN_NAME;
-    root         /var/www/$APP_USER/public;
+    root         /var/www/$APP_USER/backend/public;
 	access_log /var/log/nginx/backend-$APP_USER-access.log;
 	error_log /var/log/nginx/backend-$APP_USER-error.log;
 	index index.php index.html index.htm;
@@ -109,8 +110,8 @@ server {
 
 server {
 
-    server_name  	admin.$DOMAIN_NAME/admin/public;
-    root         /var/www/$APP_USER;
+    server_name  	admin.$DOMAIN_NAME;
+    root         /var/www/$APP_USER/admin/public;
     access_log /var/log/nginx/admin-$APP_USER-access.log;
     error_log /var/log/nginx/admin-$APP_USER-error.log;
     index index.php index.html index.htm;
@@ -174,14 +175,14 @@ set_real_ip_from 2a06:98c0::/29;
 	}
 
 
-    location /admin{
-  proxy_pass http://localhost:3000;
-  proxy_http_version 1.1;
-  proxy_set_header Upgrade \$http_upgrade;
-  proxy_set_header Connection 'upgrade';
-  proxy_set_header Host \$host;
-  proxy_cache_bypass \$http_upgrade;
-    }
+#     location /admin{
+#   proxy_pass http://localhost:3000;
+#   proxy_http_version 1.1;
+#   proxy_set_header Upgrade \$http_upgrade;
+#   proxy_set_header Connection 'upgrade';
+#   proxy_set_header Host \$host;
+#   proxy_cache_bypass \$http_upgrade;
+#     }
 #########
         location ~ \.php$ {
 
@@ -226,9 +227,10 @@ EOF
 
 ## shop Nginx config
 cat > /etc/nginx/sites-available/shop.$DOMAIN_NAME <<EOF
+
 server {
-index index.html index.htm;
-server_name shop.$DOMAIN_NAME;
+        index index.html index.htm;
+        server_name shop.$DOMAIN_NAME;
         access_log /var/log/nginx/shop-$DOMAIN_NAME-access.log;
         error_log /var/log/nginx/shop-$DOMAIN_NAME-error.log;
 location / {
@@ -252,7 +254,7 @@ location ~ /\.ht {
 
 EOF
 
-# Nginx Phpmyadmin
+# Phpmyadmin Nginx configration
 cat > /etc/nginx/sites-available/phpmyadmin <<EOF
 
 server {
@@ -295,8 +297,9 @@ ln -s /etc/nginx/sites-available/shop.$DOMAIN_NAME /etc/nginx/sites-enabled/
 rm -rf  /etc/nginx/sites-available/default 
 rm -rf /etc/nginx/sites-enabled/default 
 
+nginx -t
 
-## PhpMyAdmin
+## Install PhpMyAdmin
 
 cd /var/www/
 wget https://files.phpmyadmin.net/phpMyAdmin/5.1.2/phpMyAdmin-5.1.2-all-languages.tar.gz
@@ -306,21 +309,13 @@ mkdir -p /var/lib/phpmyadmin/tmp
 chown -R www-data:www-data /var/lib/phpmyadmin
 cp /var/www/phpmyadmin/config.sample.inc.php /var/www/phpmyadmin/config.inc.php
 apt install pwgen -y
-
 pwgen -s 32 1 > x
-
 secret=$(cat x)
-
 sed -i '/blowfish_secret/d' /var/www/phpmyadmin/config.inc.php
-
 echo "\$cfg['blowfish_secret'] = '$secret';" >> /var/www/phpmyadmin/config.inc.php
-
 echo "\$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';" >> /var/www/phpmyadmin/config.inc.php
-
 chown www-data.www-data /var/www/phpmyadmin/ -R
 rm -rf x
-
-nginx -t
 
 #### 
 
@@ -330,7 +325,7 @@ sudo apt update && sudo apt install supervisor -y
 
 
 
-# Php 8.2
+# Install Php 8.2
 sudo apt install software-properties-common -y
 
 sudo add-apt-repository ppa:ondrej/php
@@ -338,10 +333,10 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt install --no-install-recommends php8.2 -y
 
 
-sudo apt install php8.2-mbstring php8.2-xml php8.2-bcmath php8.2-simplexml php8.2-intl php8.2-gd php8.2-curl php8.2-zip php8.2-gmp composer php8.2-fpm php8.2-mysql
+sudo apt install php8.2-mbstring php8.2-xml php8.2-bcmath php8.2-simplexml php8.2-intl php8.2-gd php8.2-curl php8.2-zip php8.2-gmp composer php8.2-fpm php8.2-mysql -y
 
 
-cd /etc/php/8.1/fpm/pool.d/
+cd /etc/php/8.2/fpm/pool.d/
 
 cp www.conf phpmyadmin.conf
 
@@ -363,12 +358,13 @@ sed -i "s/user = www-data/user = $APP_USER/g" $APP_USER.conf
 sed -i "s/group = www-data/group = $APP_USER/g" $APP_USER.conf
 
 
-systemctl restart php8.1-fpm.service
+systemctl restart php8.2-fpm.service
+systemctl enable php8.2-fpm.service
 
-systemctl enable php8.1-fpm.service
 
+## Install PHP Composer
 
-## Composer
+cd /tmp
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
@@ -377,7 +373,7 @@ php -r "unlink('composer-setup.php');"
 
 
 
-#### mysql 8
+#### Install mysql 8
 
 
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.19-1_all.deb
@@ -385,6 +381,8 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.19-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.19-1_all.deb
 
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C 
+
 
 sudo apt update -y 
 
@@ -398,23 +396,15 @@ sudo systemctl start mysql
 
 sudo systemctl enable mysql
 
-sudo apt install software-properties-common -y
-
-sudo add-apt-repository ppa:ondrej/php
-
-sudo apt install --no-install-recommends php8.1 -y
-
-sudo apt install php8.1-cli php8.1-fpm php8.1-common php8.1-mysql php8.1-zip php8.1-gd  php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath -y
 
 DB_PASS=$(openssl rand -base64 12)
 
-mysql -u root -e "create database "$APP_USER"_db;" -p
+mysql -u root -e "CREATE DATABASE \`${APP_USER}_db\`;"
 
-mysql -u root -e  "CREATE USER "$APP_USER_"user@'%' IDENTIFIED BY "$DB_PASS";" -p 
+mysql -u root -e "CREATE USER '${APP_USER}_user'@'%' IDENTIFIED BY '$DB_PASS';"
 
-mysql -u root -e  "GRANT ALL PRIVILEGES ON "$APP_USER"_db.* TO "$APP_USER_"user@'%';" -p
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${APP_USER}_db\`.* TO '${APP_USER}_user'@'%';"
 
-mysql -u root -e "flush privileges;" -p
+mysql -u root -e "FLUSH PRIVILEGES;"
 
-
-echo DB-Name: "$APP_USER"_db , DB-User: "$APP_USER"_user, DB-Pass: $DB_PASS
+echo "DB-Name: ${APP_USER}_db, DB-User: ${APP_USER}_user, DB-Pass: $DB_PASS"
